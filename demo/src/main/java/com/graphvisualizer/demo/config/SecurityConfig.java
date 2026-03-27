92cec27f21fc5817db66a25bc28b2af6aa1062c6
Deepanshu Kumar <rowdyking634@gmail.com>
2026-03-27 17:55:37 +0530

    Modify CORS settings to include production URL
    Updated CORS configuration to allow requests from the production URL.

--- demo/src/main/java/com/graphvisualizer/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/graphvisualizer/demo/config/SecurityConfig.java
@@ -81,12 +81,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 // Allowed origins
         configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000",
-                "http://localhost:5500",
-                "http://localhost:8080",
-                "http://127.0.0.1:3000",
-                "http://127.0.0.1:5500",
-                "http://127.0.0.1:8080"
+                "https://magnificent-art-production.up.railway.app/"
         ));// Allowed methods
         configuration.setAllowedMethods(Arrays.asList(
                 "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"
