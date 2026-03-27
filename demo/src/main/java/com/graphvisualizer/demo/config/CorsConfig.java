eca349fccab0953f39c1af9a6545a0e92ec5455e
Deepanshu Kumar <rowdyking634@gmail.com>
2026-03-27 17:52:28 +0530

    Update CORS configuration to allow all origins

--- demo/src/main/java/com/graphvisualizer/demo/config/CorsConfig.java
+++ demo/src/main/java/com/graphvisualizer/demo/config/CorsConfig.java
@@ -7,7 +7,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
